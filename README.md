bc6ae145be3953ac9a0c1951ee6544b58fa3b4be
Nico De Witte <nico.dewitte@vives.be>
2016-12-09 14:46:35 +0100

    Start of home automation application

---
+++ README.md
@@ -0,0 +1,3 @@
+# Home Automator Application
+
+Implementation of home automator to automate a home based on mbed compatible boards backed by a hierarchical NRF24L01+ wireless network.
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+
+DigitalOut myled(LED1);
+Serial pc(USBTX, USBRX); // tx, rx
+
+int main() {
+    pc.baud(115200);
+
+    printf("Booting .... \r\n");
+
+    while(1) {
+        myled = 1;
+        wait(0.2);
+        myled = 0;
+        wait(0.2);
+    }
+}
