7b2a78eb420e2525fd5760ff9f2fe07a7490c177
Nico De Witte <nico.dewitte@vives.be>
2016-12-10 11:17:36 +0100

    Fix logit and add basic device configuration with mbed_config_parser lib

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,33 @@
 #include "LogIt.h"
 #include "LoggerInterface.h"
 
+#include "file_reader.h"
+#include "device_configuration.h"
+#include "json_device_configuration_parser.h"
+
 DigitalOut myled(LED1);
-Serial pc(USBTX, USBRX);        // tx, rx
+Serial pc(USBTX, USBRX);
+LogIt logger(&pc);
+
+void load_device_config(void) {
+  ConfigParser::FileReader reader("/local/device.jsn");
+  JsonDeviceConfigurationParser parser(&reader);
+  DeviceConfiguration *devConfig = parser.parse();
+
+  logger.info("Firmware version = %s", devConfig->version.c_str());
+  logger.debug("Description = %s", devConfig->description.c_str());
+  logger.debug("Board = %s", devConfig->board.c_str());
+
+  delete devConfig;
+}
 
 int main() {
     pc.baud(115200);
 
-    LogIt logger(&pc);
     logger.setLevel(Log::LoggerInterface::DEBUG);
-    logger.debug("Booting ....");
+    logger.info("Booting ....");
 
+    load_device_config();
 
     while(1) {
         myled = 1;
